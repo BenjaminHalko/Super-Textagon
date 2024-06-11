4b367a80597795d34f99869d56d2fdbb8efbb094
Grizzly47 <106337263+Grizzly47@users.noreply.github.com>
2024-06-11 10:32:40 -0700

    Fix: Reorder Transformation Application Order
    - Reordered how transformations are applied within the transformSystem to work as intended

--- engine/src/engine/sys/transformSystem.cpp
+++ engine/src/engine/sys/transformSystem.cpp
@@ -6,24 +6,22 @@ void TransformSystem::TranslatePoint(Point &point, float x, float y) {
     point.y += y;
 }
 
-void TransformSystem::ScalePoint(Point &point, float x_scale, float y_scale) {
-    point.x *= x_scale;
-    point.y *= y_scale;
-}
-
-void TransformSystem::RotatePoint(Point &point, float angle) {
+void TransformSystem::RotatePoint(Point& point, float angle) {
     angle *= PI / 180;
-
-    //angle = 90 * PI / 180;
 
     point.x = point.x * cosf(angle) - point.y * sinf(angle);
     point.y = point.x * sinf(angle) + point.y * cosf(angle);
 }
 
+void TransformSystem::ScalePoint(Point &point, float x_scale, float y_scale) {
+    point.x *= x_scale;
+    point.y *= y_scale;
+}
+
 void TransformSystem::ApplyTransformation(Point &point, TransformComponent &transform) {
-    TranslatePoint(point, transform.x, transform.y);
     ScalePoint(point, transform.x_scale, transform.y_scale);
     RotatePoint(point, transform.rotation);
+    TranslatePoint(point, transform.x, transform.y);
 }
 
 SpriteComponent TransformSystem::TransformSprite(SpriteComponent &sprite, TransformComponent &transform) {
