730ed854cce7d6d8fdb31d9b850b896c7daa3722
Benjamin Halko <benjaminhalko@hotmail.com>
2024-06-14 11:31:51 -0700

    feat: Linux Input

--- engine/include/engine/sys/input.h
+++ engine/include/engine/sys/input.h
@@ -3,16 +3,27 @@
 #include <engine/sys/_system.h>
 #include <unordered_map>
 
-enum Key {
+
+enum class Key {
     LEFT = 0,
     RIGHT,
-    QUIT
+    ESCAPE
 };
+
+/**
+ * @brief The Input system is used to track the state of keys
+ * @note This system should be used to check if a key is pressed
+ * or held down
+ */
 
 class Input : public System {
     friend class Engine;
     enum KeyState {UP, PRESSED, HELD};
-    static std::unordered_map<Key, KeyState> keys;
+    static std::unordered_map<Key, KeyState> _keys;
+    static std::unordered_map<Key, int> _keyMap;
+
+    static void Init();
+    static void Clean();
 
     /**
      * @brief Loops over all the keys and tracks their state
