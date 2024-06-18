ef7ca296481f9165ba3d1ca2dc0754e5751df7d2
Grizzly47 <106337263+Grizzly47@users.noreply.github.com>
2024-06-18 14:33:40 -0700

    chore: Added Comments to component files (#27)
    * feat: More components
    
    * Fix: Reorder Transformation Application Order
    
    - Reordered how transformations are applied within the transformSystem to work as intended
    
    * fix: resolved render folding in on itself
    
    - Allowed transformSystems RotatePoint function to keep track of original, allowing it to properly utilize the formula
    
    * Added Collision Logic to Triangle
    
    * fix: Fixed collision system
    
    * Added comments to all component.h files
    
    Added comments to:
    - audio.h
    - collider.h
    - depth.h
    - script.h
    - tag.h
    - timer.h
    - transform.h
    
    ---------
    
    Co-authored-by: Benjamin <benjaminhalko@hotmail.com>

--- engine/include/engine/comp/script.h
+++ engine/include/engine/comp/script.h
@@ -3,31 +3,63 @@
 #include <engine/comp/_component.h>
 #include <engine/entity.h>
 #include <any>
+#include <unordered_map>
+#include <memory>
 
+/**
+ * @brief A component for handling scripts in the engine.
+ *
+ * @details This class allows attaching and executing scripts on entities.
+ * Also provides a way to store and retrive data specific to the script.
+ */
 class Script : public Component {
 private:
-    void (*_script)(Entity&);
-    std::unordered_map<std::string, std::shared_ptr<std::any>> _data;
+    void (*_script)(Entity&); // Function pointer to the script to be executed.
+    std::unordered_map<std::string, std::shared_ptr<std::any>> _data; // Storage for script-specific data.
 public:
+    /**
+     * @brief Constructs a Script component with the given script function.
+     *
+     * @param script Function pointer to the script to be executed.
+     */
     explicit Script(void (*script)(Entity&));
+
+    /**
+     * @brief Executes the script on the given entity.
+     *
+     * @param self Reference to the entity on which the script is executed.
+     */
     void operator()(Entity& self);
 
     /**
-     * @brief Allows saving data to the script
-     * @details This is useful for situations where static would not work
-     * @tparam T
-     * @param key
-     * @param value
+     * @brief Allows saving data to the script.
+     *
+     * @tparam T The type of the data to be stored.
+     * @param key The key under which the data is stored.
+     * @param value The value to be stored.
      */
     template <typename T>
     void SetData(std::string key, T value) {
         _data[key] = std::make_shared<std::any>(value);
     }
 
+    /**
+     * @brief Retrieves data stored in the script.
+     *
+     * @tparam T The type of the data to be stored.
+     * @param key The key where the data is stored.
+     * @return Reference to the data stored under the given key.
+     */
     template <typename T>
     T& GetData(std::string key) {
         return std::any_cast<T&>(*_data[key]);
     }
 
+    /**
+     * @brief Check for data at given key.
+     *
+     * @param key The key to check for data.
+     * @return True if data exists for the given key, false otherwise.
+     */
     bool DataExists(std::string key);
 };
--- engine/include/engine/comp/transform.h
+++ engine/include/engine/comp/transform.h
@@ -7,12 +7,45 @@
  */
 class Transform : public Component {
 public:
-    float x, y;
+    float x, y; 
     float x_scale = 1, y_scale = 1;
-    float rotation = 0;
+    float rotation = 0; 
 
+    /**
+     * @brief Constructs a Transform with the given position.
+     *
+     * @param x The x-coordinate of the position.
+     * @param y The y-coordinate of the position.
+     */
     Transform(float x, float y);
+
+    /**
+     * @brief Constructs a Transform with the given position and scale.
+     *
+     * @param x The x-coordinate of the position.
+     * @param y The y-coordinate of the position.
+     * @param x_scale The scale factor along the x-axis.
+     * @param y_scale The scale factor along the y-axis.
+     */
     Transform(float x, float y, float x_scale, float y_scale);
+
+    /**
+     * @brief Constructs a Transform with the given position and rotation.
+     *
+     * @param x The x-coordinate of the position.
+     * @param y The y-coordinate of the position.
+     * @param rotation The rotation angle in degrees.
+     */
     Transform(float x, float y, float rotation);
+
+    /**
+     * @brief Constructs a Transform with the given position, scale, and rotation.
+     *
+     * @param x The x-coordinate of the position.
+     * @param y The y-coordinate of the position.
+     * @param x_scale The scale factor along the x-axis.
+     * @param y_scale The scale factor along the y-axis.
+     * @param rotation The rotation angle in degrees.
+     */
     Transform(float x, float y, float x_scale, float y_scale, float rotation);
 };
